1fdc8c620814d2892e43939f044808b78fd38d8b
AASTHA <72413064+AasthaCodex@users.noreply.github.com>
2021-04-17 15:23:59 +0530

    Create question1.cpp

---
+++ answers/AasthaCodex/Day 30/question1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    map <string,string> we;
+    for(int i=0;i<m;i++)
+    {
+        string p,q;
+        cin>>p>>q;
+        we[p]=q;
+    }
+    for(int i=0;i<n;i++)
+    {
+        string s;
+        cin>>s;
+        string t;
+        t=t[s];
+        if(y.size()<x.size())
+            cout<<t<<" ";
+        else
+            cout<<s<<" ";
+    }
+    return 0;
+}
